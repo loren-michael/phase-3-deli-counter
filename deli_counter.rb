ed15975ecbb6574fe2ff87e8d8d90618a80bb486
loren <lorenmichaelh@gmail.com>
2022-07-13 15:49:06 -0500

    completed assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts 'The line is currently empty.'
+    else
+        line_text = 'The line is currently:'
+        katz_deli.each do |name|
+            line_text << " #{katz_deli.find_index(name) + 1}. #{name}"
+        end
+        puts line_text
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts 'There is nobody waiting to be served!'
+    else
+        puts "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
+    end
+end
+
+# binding.pry
+# 0
